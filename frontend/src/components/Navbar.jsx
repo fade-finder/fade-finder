74b27334db10364190c36a795d54539eb1dcc90b
Daniel Solis <daniel.solis5200@gmail.com>
2023-03-22 09:05:26 -0600

    Se corrigio las rutas del Navbar para que solo se muestren algunos Link cuando este en la ruta /

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,75 +1,76 @@
 import { Link } from 'react-scroll'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
 
 const Navbar = () => {
+	const location = useLocation()
 	return (
 		<nav className=''>
 			<ul className='flex gap-x-5'>
 				<li>
-					<Link
-						className='cursor-pointer text-black font-medium'
-						activeClass='active'
-						to='inicio'
-						spy={true}
-						smooth={true}
-						offset={-180}
-						duration={500}
-					>
-						Inicio
-					</Link>
+					{location.pathname == '/' ? (
+						<Link
+							className='cursor-pointer text-black font-medium'
+							activeClass='active'
+							to='inicio'
+							spy={true}
+							smooth={true}
+							offset={-180}
+							duration={500}
+						>
+							Inicio
+						</Link>
+					) : (
+						<NavLink
+							to='/'
+							className={({ isActive }) => (isActive ? 'active' : '')}
+						>
+							Inicio
+						</NavLink>
+					)}
 				</li>
-				<li>
-					<Link
-						className='cursor-pointer text-black font-medium'
-						activeClass='active'
-						to='servicios'
-						spy={true}
-						smooth={true}
-						offset={-180}
-						duration={500}
-					>
-						Servicios
-					</Link>
-				</li>
-				<li>
-					<Link
-						className='cursor-pointer text-black font-medium'
-						activeClass='active'
-						to='galeria'
-						spy={true}
-						smooth={true}
-						offset={-180}
-						duration={500}
-					>
-						Galeria
-					</Link>
-				</li>
-				{/* <li>
-					<Link
-						className='cursor-pointer text-black font-medium'
-						activeClass='active'
-						to='quienes-somos'
-						spy={true}
-						smooth={true}
-						offset={-100}
-						duration={500}
-					>
-						Quiénes somos
-					</Link>
-				</li> */}
-				<li>
-					<Link
-						className='cursor-pointer text-black font-medium'
-						activeClass='active'
-						to='contactanos'
-						spy={true}
-						smooth={true}
-						offset={-180}
-						duration={500}
-					>
-						Contactanos
-					</Link>
-				</li>
+				{location.pathname === '/' && (
+					<>
+						<li>
+							<Link
+								className='cursor-pointer text-black font-medium'
+								activeClass='active'
+								to='servicios'
+								spy={true}
+								smooth={true}
+								offset={-180}
+								duration={500}
+							>
+								Servicios
+							</Link>
+						</li>
+						<li>
+							<Link
+								className='cursor-pointer text-black font-medium'
+								activeClass='active'
+								to='galeria'
+								spy={true}
+								smooth={true}
+								offset={-180}
+								duration={500}
+							>
+								Galeria
+							</Link>
+						</li>
+						<li>
+							<Link
+								className='cursor-pointer text-black font-medium'
+								activeClass='active'
+								to='contactanos'
+								spy={true}
+								smooth={true}
+								offset={-180}
+								duration={500}
+							>
+								Contactanos
+							</Link>
+						</li>
+					</>
+				)}
 				<li>
 					<NavLink
 						to='/admin/negocio'
