6985a2149690ea4433cd124ee13805b39994ec0e
Bryan <bryan120419v@gmail.com>
2023-03-23 22:23:54 -0600

    Se cambia el modelo de Barbero por Usuario, se modifican las funciones del controlador admin para el nuevo modelo y se agrega el nuevo script de la base de datos

--- backend/utils/ScriptBaseDeDatos.sql
+++ backend/utils/ScriptBaseDeDatos.sql
@@ -3,39 +3,39 @@ Create database FadeFinder;
 use FadeFinder;
 
 /* Se crean las tablas */
-create table cliente(
-	idCliente int not null unique auto_increment primary key,
-    nombre varchar(45),
-    ap_paterno varchar(45),
-    ap_materno varchar(45),
-    fecha_nacimiento date,
-    telefono varchar(10),
-	correo varchar(45),
-    password varchar(45),
-    direccion varchar(45),
-    foto varchar(100)
+create table rol(
+	idRol int not null unique primary key,
+    nombre varchar(30)
 );
 
-create table barbero(
-	idBarbero int not null unique auto_increment primary key,
+create table usuario(
+	idUsuario int not null unique auto_increment primary key,
     nombre varchar(45),
     ap_paterno varchar(45),
     ap_materno varchar(45),
-    correo varchar(45),
+    correo varchar(45) unique,
     password varchar(45),
-    genero char(1),
     telefono varchar(10),
     foto varchar(100),
-    direccion varchar(45),
-    fecha_nacimiento date,
-    estado int(1)
+    estado int(1),
+    idRol int,
+    foreign key (idRol) references rol (idRol)
+);
+
+create table horario(
+	idHorario int not null auto_increment primary key,
+    dia varchar(45),
+    hora_inicio time,
+    hora_fin time,
+    idUsuario int,
+    foreign key (idUsuario) references usuario (idUsuario)
 );
 
 create table servicio(
 	idServicio int not null unique auto_increment primary key,
     nombre varchar(45),
     precio float,
-    descripcion varchar(50),
+    descripcion varchar(200),
     duracion int,
     disponibilidad int(1),
     imagen varchar(100)
@@ -48,33 +48,8 @@ create table cita(
     horario datetime,
     duracion int,
     total_pagar float,
-    idBarbero int,
-    idCliente int,
-    foreign key (idBarbero) references barbero (idBarbero),
-    foreign key (idCliente) references cliente (idCliente)
-);
-
-create table horario(
-	idHorario int not null auto_increment primary key,
-    dia varchar(45),
-    hora_inicio time,
-    hora_fin time,
-    idBarbero int,
-    foreign key (idBarbero) references barbero (idBarbero)
-);
-
-create table admin(
-	idAdmin int not null unique auto_increment primary key,
-    nombre varchar(45),
-    ap_paterno varchar(45),
-    ap_materno varchar(45),
-    correo varchar(45),
-    password varchar(45),
-    genero char(1),
-    telefono varchar(10),
-    foto varchar(100),
-    direccion varchar(45),
-    fecha_nacimiento date
+    idUsuario int,
+    foreign key (idUsuario) references usuario (idUsuario)
 );
 
 create table cita_servicio(
@@ -85,4 +60,12 @@ create table cita_servicio(
     primary key (idCita,idServicio)
 );
 
-select * from barbero;
+-- Insercion de roles
+insert into rol (idRol, nombre) values (1, 'cliente');
+insert into rol (idRol, nombre) values (2, 'barbero');
+insert into rol (idRol, nombre) values (3, 'admin');
+
+insert into usuario values (null, 'Kike', 'Camarena', 'Garcia', 'kike@gmail.com', '1234', '1234567890', '', 1, 1);
+
+select * from usuario;
+
